f19258601c1dcf4fdb5c522c962ca45a2af62a34
roya parsa <99053997+rparsa49@users.noreply.github.com>
2023-11-26 17:21:59 -0500

    changes

--- group_assignment/post_grades.php
+++ group_assignment/post_grades.php
@@ -1,46 +1,56 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>PHP Connect MySQL Database</title>
-    </head>
-    <body>
-        <h1>PHP Connect MySQL Database</h1>
-        <p><?php
-                $servername = "localhost";
-                $username = "root";
-                $password = "AlfieHershey";
-                $database = "grades";
-                $port = "3308";
+<head>
+    <title>PHP Connect MySQL Database</title>
+</head>
+<body>
+    <h1>PHP Connect MySQL Database</h1>
+    <p><?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "AlfieHershey";
+        $database = "grades";
+        $port = "3308";
 
-                $conn = new mysqli($servername, $username, $password, $database, $port);
+        $conn = new mysqli($servername, $username, $password, $database, $port);
 
-                if ($conn->connect_error) {
-                    die("<p style='color:red'>" . "Connection Failed: " . $conn->connect_error . "</p>");
-                }
+        if ($conn->connect_error) {
+            die("<p style='color:red'>" . "Connection Failed: " . $conn->connect_error . "</p>");
+        }
 
-                //echo "MySQL DB Connected successfully... <br>";
+        //echo "MySQL DB Connected successfully... <br>";
 
-                    $courseid = $_POST['courseid'];
-                    $studentid = $_POST['studentid'];
-                    $term = $_POST['term'];
-                    $grade = $_POST['grade'];
-                
-                    $sql = "INSERT INTO `GRADES` VALUES('$courseid', '$studentid', '$term', '$grade');";
+        $coursename = $_POST['coursename'];
+        $credits = $_POST['credits'];
+        $studentid = $_POST['studentid'];
+        $term = $_POST['term'];
+        $grade = $_POST['grade'];
 
-                    if($insert = $conn->query($sql) === TRUE) {
-                        echo "Grades successfully uploaded!";
-                    }
-                    else {
-                        echo "Error!";
-                    }
+        // Insert course into COURSES table
+        $insertCourseSQL = "INSERT INTO `COURSES` (coursename, credits) VALUES ('$coursename', '$credits');";
+        if ($conn->query($insertCourseSQL) === TRUE) {
+            // Retrieve the courseid of the inserted course
+            $courseid = $conn->insert_id;
 
-                $conn->close();
+            // Insert grade into GRADES table
+            $insertGradeSQL = "INSERT INTO `GRADES` (courseid, studentid, term, grade) VALUES ('$courseid', '$studentid', '$term', '$grade');";
 
-                //echo "<br> DB Disconnect";
-        ?></p>
+            if ($conn->query($insertGradeSQL) === TRUE) {
+                echo "Grades successfully uploaded!";
+            } else {
+                echo "Error inserting grade: " . $conn->error;
+            }
+        } else {
+            echo "Error inserting course: " . $conn->error;
+        }
 
-        <div>
-            <a href="home.html">Return Home</a>
-        </div>
-    </body>
+        $conn->close();
+
+        //echo "<br> DB Disconnect";
+    ?></p>
+
+    <div>
+        <a href="home.html">Return Home</a>
+    </div>
+</body>
 </html>
